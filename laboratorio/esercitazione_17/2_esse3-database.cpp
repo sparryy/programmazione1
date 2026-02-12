bfc521f006c8c221ecf8f112f445d4a165624bd0
Sparry <leonardo.sparano@studenti.unitn.it>
2026-02-12 20:48:38 +0000

    17.2 fatto grazie micol

--- laboratorio/esercitazione_17/2_esse3-database.cpp
+++ laboratorio/esercitazione_17/2_esse3-database.cpp
@@ -11,7 +11,7 @@ struct Studente
     float media;
 };
 
-Studente **genera_database(int length);
+Studente **genera_database(int length, char * &fileName);
 
 Studente *genera_studente(char nome[], char cognome[], int matricola, float media_ponderata);
 
@@ -24,15 +24,21 @@ int main(int argc, char *argv[])
         exit(0);
     }
 
-    fstream input;
-    input.open(argv[1], ios::in);
-
-    if (input.fail()) {
-        cout << "Il file dato in input " << argv[1] << " non esiste!" << endl;
-        exit(0);
-    }
-    
+    char *fileName = argv[1];
     int length = atof(argv[2]);
+
+    Studente **database = genera_database(length, fileName);
+
+    for(int i = 0; i < length; i++) {
+        stampa_studente(database[i]);
+        cout << endl;
+    }
+
+    for(int i = 0; i < length; i++) {
+        delete[] database[i];
+    }
+
+    delete[] database;
 
     return 0;
 }
@@ -55,6 +61,30 @@ void stampa_studente(Studente *studente)
     cout << "Media: " << studente->media << endl;
 }
 
-Studente **genera_database(int length) {
+Studente **genera_database(int length, char * &fileName) {
     Studente **database = new Studente*[length];
+
+    fstream input;
+    input.open(fileName, ios::in);
+
+    if (input.fail()) {
+        cout << "Il file dato in input " << fileName << " non esiste!" << endl;
+        exit(0);
+    }
+
+    char name[256];
+    char cognome[256];
+    int  matricola;
+    float media;
+    
+    for(int i = 0; i < length; i++) {
+        input >> name;
+        input >> cognome;
+        input >> matricola;
+        input >> media;
+        database[i] = genera_studente(name, cognome, matricola, media);
+    }
+
+    input.close();
+    return database;
 }
